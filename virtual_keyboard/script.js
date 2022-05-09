288acc519362071a0d40b2e1751a075e52968409
Roman Patsiaichuk <roman.shady@yandex.ru>
2022-05-10 02:07:35 +0300

    feat: added the functionality of clicking on the physical keyboard - Tab, BackSpace, Del, Enter

--- virtual_keyboard/script.js
+++ virtual_keyboard/script.js
@@ -2,6 +2,26 @@ const wrapper = document.querySelectorAll(".keyboard_wrapper> div");
 let i = 1;
 let cursor_position = null;
 
+let servises = [
+  "Tab",
+  "ShiftLeft",
+  "CapsLock",
+  "ControlLeft",
+  "MetaLeft",
+  "AltLeft",
+  "Space",
+  "AltRight",
+  "ArrowUp",
+  "ArrowDown",
+  "ArrowLeft",
+  "ArrowRight",
+  "ControlRight",
+  "ShiftRight",
+  "Enter",
+  "Delete",
+  "Backspace",
+];
+
 document.addEventListener("DOMContentLoaded", () => {
   textarea.focus();
 });
@@ -14,6 +34,10 @@ textarea.addEventListener("click", (event) => {
   cursor_position = getCaretPos();
 });
 
+textarea.addEventListener("keydown", (event) => {
+  event.preventDefault();
+});
+
 wrapper.forEach((element) => {
   element.addEventListener("click", (event) => {
     let elem = event.target;
@@ -21,23 +45,23 @@ wrapper.forEach((element) => {
     if (!elem.classList.contains("services_btn")) {
       printChar(event.target.innerHTML);
     }
-    if (elem.innerHTML === "Backspace") {
+    if (elem.classList.contains("Backspace")) {
       Backspace();
     }
 
-    if (elem.innerHTML === "Tab") {
+    if (elem.classList.contains("Tab")) {
       printChar("\t");
     }
 
-    if (elem.innerHTML === "Space") {
+    if (elem.classList.contains("Space")) {
       printChar(" ");
     }
 
-    if (elem.innerHTML === "Enter") {
+    if (elem.classList.contains("Enter")) {
       printChar("\n");
     }
 
-    if (elem.innerHTML === "Del") {
+    if (elem.classList.contains("Delete")) {
       textarea.innerHTML =
         textarea.innerHTML.slice(0, cursor_position) +
         textarea.innerHTML.slice(
@@ -46,6 +70,49 @@ wrapper.forEach((element) => {
         );
       textarea.selectionStart = cursor_position;
     }
+  });
+});
+
+textarea.addEventListener("keydown", (event) => {
+  wrapper.forEach((elem) => {
+    if (elem.classList.contains(event.code)) {
+      elem.classList.add("active");
+      if (!elem.classList.contains("services_btn")) {
+        printChar(elem.innerHTML);
+      }
+
+      if (elem.classList.contains("Backspace")) {
+        Backspace();
+      }
+
+      if (elem.classList.contains("Tab")) {
+        printChar("\t");
+      }
+
+      if (elem.classList.contains("Space")) {
+        printChar(" ");
+      }
+
+      if (elem.classList.contains("Enter")) {
+        printChar("\n");
+      }
+
+      if (elem.classList.contains("Delete")) {
+        textarea.innerHTML =
+          textarea.innerHTML.slice(0, cursor_position) +
+          textarea.innerHTML.slice(
+            cursor_position + 1,
+            textarea.innerHTML.length
+          );
+        textarea.selectionStart = cursor_position;
+      }
+    }
+  });
+});
+
+textarea.addEventListener("keyup", (event) => {
+  wrapper.forEach((element) => {
+    element.classList.remove("active");
   });
 });
 
