c55ab8d12d849da0a511bda95c2b0a723aab878e
Roman Patsiaichuk <roman.shady@yandex.ru>
2022-05-10 18:44:38 +0300

    feat: added "ru" and "en" lang

--- virtual_keyboard/script.js
+++ virtual_keyboard/script.js
@@ -1,6 +1,18 @@
 const wrapper = document.querySelectorAll(".keyboard_wrapper> div");
 let i = 1;
 let cursor_position = null;
+
+console.log(navigator.appVersion);
+
+function getOS() {
+  if (navigator.appVersion.indexOf("Windows") >= 0) return "Windows";
+  if (navigator.appVersion.indexOf("Linux") >= 0) return "Linux";
+  if (navigator.appVersion.indexOf("Sun") == 0) return "SunOS";
+  if (navigator.appVersion.indexOf("Mac") == 0) return "MacOS";
+}
+
+init_os.innerHTML = `Клавиатура создана в операционной системе: ${getOS()}`;
+
 alert(
   "Привет. Нужно кое-что допилить. Пожалуйста не спиши проверять. Крайний срок вроде как до четверга включительно. Вот мой телеграмм для связи https://t.me/Roman914"
 );
@@ -23,6 +35,106 @@ alert(
 //   "Delete",
 //   "Backspace",
 // ];
+
+let letters_ru = [
+  "ё",
+  "й",
+  "ц",
+  "у",
+  "к",
+  "е",
+  "н",
+  "г",
+  "ш",
+  "щ",
+  "з",
+  "х",
+  "ъ",
+  "ф",
+  "ы",
+  "в",
+  "а",
+  "п",
+  "р",
+  "о",
+  "л",
+  "д",
+  "ж",
+  "э",
+  "я",
+  "ч",
+  "с",
+  "м",
+  "и",
+  "т",
+  "ь",
+  "б",
+  "ю",
+  ".",
+];
+let letters_en = [
+  "`",
+  "q",
+  "w",
+  "e",
+  "r",
+  "t",
+  "y",
+  "u",
+  "i",
+  "o",
+  "p",
+  "[",
+  "]",
+  "a",
+  "s",
+  "d",
+  "f",
+  "g",
+  "h",
+  "j",
+  "k",
+  "l",
+  ";",
+  "'",
+  "z",
+  "x",
+  "c",
+  "v",
+  "b",
+  "n",
+  "m",
+  ",",
+  ".",
+  "/",
+];
+const ru = document.querySelectorAll('[class*="ru"]');
+console.log(ru);
+let val = false;
+
+function ru_lang() {
+  if (val) {
+    ru.forEach((element, index) => {
+      element.innerHTML = letters_en[index];
+    });
+
+    val = false;
+  } else {
+    ru.forEach((element, index) => {
+      element.innerHTML = letters_ru[index];
+    });
+    val = true;
+  }
+}
+
+CtrlLeft.addEventListener("click", ru_lang);
+
+/*
+1) русские клавиши класс "ru", querySelector взять +
+2) Пройтись через цикл и менять сочетанием клавиш +
+3) Shift - вызывать функцию Caps и присвоить клавиши из массива. servises_ru, services_en
+
+*/
 
 document.addEventListener("DOMContentLoaded", () => {
   textarea.focus();
@@ -139,6 +251,10 @@ textarea.addEventListener("keydown", (event) => {
           }
           elem.dataset.val = "true";
         }
+      }
+
+      if (elem.classList.contains("ControlLeft")) {
+        ru_lang();
       }
     }
   });
